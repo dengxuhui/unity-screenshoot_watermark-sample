946af3515143f623b679241a09f3580d0c0c5639
dengxuhui <419270388@qq.com>
2023-11-24 09:37:50 +0800

    Android监听文件目录变更

---
+++ ScreenShootWatermark/Assets/Scripts/Platform/ShotScreenListener.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 截图监听器
+/// </summary>
+public class ShotScreenListener : MonoBehaviour
+{
+    
+}
